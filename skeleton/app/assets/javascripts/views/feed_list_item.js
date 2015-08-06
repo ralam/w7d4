aaf4821a073317f00cd3fde3aa8e9f0f855c3307
ralam <rehan.alam@gmail.com>
2015-08-06 15:48:25 -0700

    Add delete feed button

--- skeleton/app/assets/javascripts/views/feed_list_item.js
+++ skeleton/app/assets/javascripts/views/feed_list_item.js
@@ -3,8 +3,17 @@ NewsReader.Views.FeedListItem = Backbone.View.extend({
 
   tagName: 'li',
 
+  events: {
+    "click button.destroy": "destroyFeed"
+  },
+
   render: function () {
     this.$el.html(this.template({feed: this.model}));
     return this;
+  },
+
+  destroyFeed: function (e) {
+    this.model.destroy();
+    this.remove();
   }
 });
